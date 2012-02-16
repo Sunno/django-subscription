7d0194fe0b7ea7daf12e815c56b47ef1a7c67452
Celso Pinto <celso@leftfield.pt>
2012-02-16 18:25:41 +0000

    removed old migrations (not needed anyways)

--- subscription/migrations/0001_initial.py
+++ subscription/migrations/0001_initial.py
@@ -1,130 +1,112 @@
-
+# encoding: utf-8
+import datetime
 from south.db import db
+from south.v2 import SchemaMigration
 from django.db import models
-from subscription.models import *
 
-class Migration:
-    
+class Migration(SchemaMigration):
+
     def forwards(self, orm):
-        
-        # Adding model 'UserSubscription'
-        db.create_table('subscription_usersubscription', (
-            ('id', orm['subscription.UserSubscription:id']),
-            ('user', orm['subscription.UserSubscription:user']),
-            ('subscription', orm['subscription.UserSubscription:subscription']),
-            ('expires', orm['subscription.UserSubscription:expires']),
-            ('active', orm['subscription.UserSubscription:active']),
-            ('cancelled', orm['subscription.UserSubscription:cancelled']),
-        ))
-        db.send_create_signal('subscription', ['UserSubscription'])
-        
-        # Adding model 'Transaction'
-        db.create_table('subscription_transaction', (
-            ('id', orm['subscription.Transaction:id']),
-            ('timestamp', orm['subscription.Transaction:timestamp']),
-            ('subscription', orm['subscription.Transaction:subscription']),
-            ('user', orm['subscription.Transaction:user']),
-            ('event', orm['subscription.Transaction:event']),
-            ('amount', orm['subscription.Transaction:amount']),
-            ('comment', orm['subscription.Transaction:comment']),
-        ))
-        db.send_create_signal('subscription', ['Transaction'])
         
         # Adding model 'Subscription'
         db.create_table('subscription_subscription', (
-            ('id', orm['subscription.Subscription:id']),
-            ('name', orm['subscription.Subscription:name']),
-            ('description', orm['subscription.Subscription:description']),
-            ('price', orm['subscription.Subscription:price']),
-            ('recurrence_period', orm['subscription.Subscription:recurrence_period']),
-            ('recurrence_unit', orm['subscription.Subscription:recurrence_unit']),
-            ('group', orm['subscription.Subscription:group']),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=64, decimal_places=2)),
+            ('trial_period', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+            ('trial_unit', self.gf('django.db.models.fields.CharField')(max_length=1, null=True)),
+            ('recurrence_period', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+            ('recurrence_unit', self.gf('django.db.models.fields.CharField')(max_length=1, null=True)),
+            ('group', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.Group'])),
         ))
         db.send_create_signal('subscription', ['Subscription'])
-        
-        # Creating unique_together for [user, subscription] on UserSubscription.
+
+        # Adding model 'UserSubscription'
+        db.create_table('subscription_usersubscription', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('subscription', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['subscription.Subscription'])),
+            ('expires', self.gf('django.db.models.fields.DateField')(default=datetime.date.today, null=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('cancelled', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal('subscription', ['UserSubscription'])
+
+        # Adding unique constraint on 'UserSubscription', fields ['user', 'subscription']
         db.create_unique('subscription_usersubscription', ['user_id', 'subscription_id'])
-        
-    
-    
+
+
     def backwards(self, orm):
         
-        # Deleting model 'UserSubscription'
-        db.delete_table('subscription_usersubscription')
-        
-        # Deleting model 'Transaction'
-        db.delete_table('subscription_transaction')
-        
+        # Removing unique constraint on 'UserSubscription', fields ['user', 'subscription']
+        db.delete_unique('subscription_usersubscription', ['user_id', 'subscription_id'])
+
         # Deleting model 'Subscription'
         db.delete_table('subscription_subscription')
-        
-        # Deleting unique_together for [user, subscription] on UserSubscription.
-        db.delete_unique('subscription_usersubscription', ['user_id', 'subscription_id'])
-        
-    
-    
+
+        # Deleting model 'UserSubscription'
+        db.delete_table('subscription_usersubscription')
+
+
     models = {
         'auth.group': {
+            'Meta': {'object_name': 'Group'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
         'auth.permission': {
-            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
         'auth.user': {
+            'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
         'contenttypes.contenttype': {
-            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         'subscription.subscription': {
+            'Meta': {'ordering': "('price', '-recurrence_period')", 'object_name': 'Subscription'},
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'group': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.Group']", 'unique': 'True'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.Group']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
             'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '64', 'decimal_places': '2'}),
             'recurrence_period': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'recurrence_unit': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True'})
-        },
-        'subscription.transaction': {
-            'amount': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '64', 'decimal_places': '2', 'blank': 'True'}),
-            'comment': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
-            'event': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'subscription': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['subscription.Subscription']", 'null': 'True', 'blank': 'True'}),
-            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+            'recurrence_unit': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True'}),
+            'trial_period': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'trial_unit': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True'})
         },
         'subscription.usersubscription': {
-            'Meta': {'unique_together': "(('user', 'subscription'),)"},
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'cancelled': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'Meta': {'unique_together': "(('user', 'subscription'),)", 'object_name': 'UserSubscription'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'cancelled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'expires': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today', 'null': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'subscription': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['subscription.Subscription']"}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
         }
     }
-    
+
     complete_apps = ['subscription']
