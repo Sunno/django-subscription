e76a0c09481d97d6a1920d2326d458a686940b13
Simón Castillo <scastb@gmail.com>
2012-10-25 12:26:47 -0430

    slight modificatin on the decorator to check for expired and canceled subscription

--- subscription/decorators.py
+++ subscription/decorators.py
@@ -4,14 +4,22 @@ from django.conf import settings
 
 SUBSCRIPTION_START_URL = getattr(settings, 'SUBSCRIPTION_START_URL', None)
 
+def valid_helper(u):
+    current = u.get_active_subscription()
+    return current is not None and current.valid() and \
+        not current.expired() and not current.cancelled
+
 def subscription_required(function=None,
                           redirect_field_name=REDIRECT_FIELD_NAME,
-                          login_url=SUBSCRIPTION_START_URL):
+                          subscription_url=SUBSCRIPTION_START_URL,
+                          login_url=settings.LOGIN_URL):
     """Decorator for views that checks if the current user has
     *any* active subscription"""
+
+
     actual_decorator = user_passes_test(
-        lambda u: u.get_active_subscription() is not None or u.is_coach or u.is_superuser,
-        login_url=login_url,
+        lambda u: (valid_helper(u) or u.is_coach or u.is_superuser),
+        login_url=subscription_url,
         redirect_field_name=redirect_field_name
     )
     if function:
